e108b801db0bd55498ac3ee762077776f33152fb
Marian Figula <majofigulam3@gmail.com>
2024-10-30 10:28:58 +0100

    feat: created AuthInterceptor

---
+++ app/src/main/java/eu/mcomputing/mobv/mobvzadanie/AuthInterceptor.kt
@@ -0,0 +1,24 @@
+package eu.mcomputing.mobv.mobvzadanie
+
+import android.content.Context
+import eu.mcomputing.mobv.mobvzadanie.config.AppConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private val context: Context) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+            .newBuilder()
+            .addHeader("Accept", "application/json")
+            .addHeader("Content-Type", "application/json")
+
+
+        val token = PreferenceData.getInstance().getUser(context)?.access
+        request.header("Authorization","Bearer $token")
+
+        // add api key to each request
+        request.addHeader("x-apikey", AppConfig.API_KEY)
+
+        return chain.proceed(request.build())
+    }
+}
