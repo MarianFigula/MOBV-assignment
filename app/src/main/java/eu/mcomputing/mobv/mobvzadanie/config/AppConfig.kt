15049e96881190dfe99a2a5d4ea118edd4d5e21d
Marian Figula <majofigulam3@gmail.com>
2024-10-30 19:30:39 +0100

    feat: added SharedPreferences_KEY

--- app/src/main/java/eu/mcomputing/mobv/mobvzadanie/config/AppConfig.kt
+++ app/src/main/java/eu/mcomputing/mobv/mobvzadanie/config/AppConfig.kt
@@ -3,4 +3,5 @@ package eu.mcomputing.mobv.mobvzadanie.config
 object AppConfig {
     const val API_KEY = "c95332ee022df8c953ce470261efc695ecf3e784" // Replace with your actual API key
     const val BASE_URL = "https://your.api.base.url" // Optional: base URL for your API
+    const val SharedPreferences_KEY = "eu.mcomputing.mobv.mobvzadanie"
 }
