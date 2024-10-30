2c481173dff89151bee5b4b4e97aa269d5c904c8
Marian Figula <majofigulam3@gmail.com>
2024-10-30 09:33:31 +0100

    feat: added toJson and fromJson functions

--- app/src/main/java/eu/mcomputing/mobv/mobvzadanie/User.kt
+++ app/src/main/java/eu/mcomputing/mobv/mobvzadanie/User.kt
@@ -1,9 +1,33 @@
 package eu.mcomputing.mobv.mobvzadanie
 
+import com.google.gson.Gson
+import java.io.IOException
+
 data class User(
     val username: String,
     val email: String,
-    val uid: String,
+    val id: String,
     val access: String,
     val refresh: String
-)
+) {
+
+    fun toJson(): String? {
+        return try {
+            Gson().toJson(this)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            null
+        }
+    }
+
+    companion object {
+        fun fromJson(string: String): User? {
+            return try {
+                Gson().fromJson(string, User::class.java)
+            } catch (ex: IOException) {
+                ex.printStackTrace()
+                null
+            }
+        }
+    }
+}
