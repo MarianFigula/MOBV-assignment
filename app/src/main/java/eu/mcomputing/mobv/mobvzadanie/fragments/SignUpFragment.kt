b163f66ebb5a3ab23b72f90f4e2f0a6d6ead5f30
Marian Figula <majofigulam3@gmail.com>
2024-10-30 19:45:55 +0100

    feat: added already logged in

--- app/src/main/java/eu/mcomputing/mobv/mobvzadanie/fragments/SignUpFragment.kt
+++ app/src/main/java/eu/mcomputing/mobv/mobvzadanie/fragments/SignUpFragment.kt
@@ -2,10 +2,12 @@ package eu.mcomputing.mobv.mobvzadanie.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import eu.mcomputing.mobv.mobvzadanie.R
 import eu.mcomputing.mobv.mobvzadanie.data.DataRepository
@@ -13,7 +15,7 @@ import eu.mcomputing.mobv.mobvzadanie.data.PreferenceData
 import eu.mcomputing.mobv.mobvzadanie.databinding.FragmentSignupBinding
 import eu.mcomputing.mobv.mobvzadanie.viewmodels.AuthViewModel
 
-class SignupFragment : Fragment(R.layout.fragment_signup) {
+class SignUpFragment : Fragment(R.layout.fragment_signup) {
     private lateinit var viewModel: AuthViewModel
     private var binding: FragmentSignupBinding? = null
 
@@ -29,6 +31,8 @@ class SignupFragment : Fragment(R.layout.fragment_signup) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val alreadyLoggedIn: TextView = view.findViewById(R.id.alreadyLoggedInLink)
 
         binding = FragmentSignupBinding.bind(view).apply {
             lifecycleOwner = viewLifecycleOwner
@@ -52,6 +56,11 @@ class SignupFragment : Fragment(R.layout.fragment_signup) {
                 } ?: PreferenceData.getInstance().putUser(requireContext(), null)
             }
         }
+
+        alreadyLoggedIn.setOnClickListener {
+            findNavController().navigate(R.id.forgotPasswordFragment)
+        }
+
     }
 
 }
