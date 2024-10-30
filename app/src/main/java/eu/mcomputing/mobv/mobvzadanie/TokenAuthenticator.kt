cfa9a9236984c8a185aae1c97590da30081f7365
Marian Figula <majofigulam3@gmail.com>
2024-10-30 10:29:05 +0100

    feat: created TokenAuthenticator

---
+++ app/src/main/java/eu/mcomputing/mobv/mobvzadanie/TokenAuthenticator.kt
@@ -0,0 +1,45 @@
+package eu.mcomputing.mobv.mobvzadanie
+
+import android.content.Context
+import eu.mcomputing.mobv.mobvzadanie.data.api.ApiService
+import eu.mcomputing.mobv.mobvzadanie.data.api.RefreshTokenRequest
+import eu.mcomputing.mobv.mobvzadanie.data.model.User
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Route
+
+class TokenAuthenticator(val context: Context) : Authenticator {
+    override fun authenticate(route: Route?, response: okhttp3.Response): Request? {
+
+
+        if (response.code == 401) {
+            val userItem = PreferenceData.getInstance().getUser(context)
+            userItem?.let { user ->
+                val tokenResponse = ApiService.create(context).refreshTokenBlocking(
+                    RefreshTokenRequest(user.refresh)
+                ).execute()
+
+                if (tokenResponse.isSuccessful) {
+                    tokenResponse.body()?.let {
+                        val newUser = User(
+                            user.username,
+                            user.email,
+                            user.id,
+                            it.access,
+                            it.refresh,
+                            user.photo
+                        )
+                        PreferenceData.getInstance().putUser(context, newUser)
+                        return response.request.newBuilder()
+                            .header("Authorization", "Bearer ${newUser.access}")
+                            .build()
+                    }
+                }
+            }
+            //if there was no success of refresh token we logout user and clean any data
+            PreferenceData.getInstance().clearData(context)
+            return null
+        }
+        return null
+    }
+}
