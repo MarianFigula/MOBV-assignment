42cda40f2836319ce7c856efd0593787bfc0f381
Marian Figula <majofigulam3@gmail.com>
2024-10-30 09:56:50 +0100

    feat: created AppConfig

---
+++ app/src/main/java/eu/mcomputing/mobv/mobvzadanie/config/AppConfig.kt
@@ -0,0 +1,6 @@
+package eu.mcomputing.mobv.mobvzadanie.config
+
+object AppConfig {
+    const val API_KEY = "c95332ee022df8c953ce470261efc695ecf3e784" // Replace with your actual API key
+    const val BASE_URL = "https://your.api.base.url" // Optional: base URL for your API
+}
