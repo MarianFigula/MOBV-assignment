3286b2a8405d739cfe8b5b1bebdaf95a1312bf5d
Marian Figula <majofigulam3@gmail.com>
2024-10-30 12:17:36 +0100

    fix: implemented feed fragment

--- app/src/main/java/eu/mcomputing/mobv/mobvzadanie/fragments/FeedFragment.kt
+++ app/src/main/java/eu/mcomputing/mobv/mobvzadanie/fragments/FeedFragment.kt
@@ -13,51 +13,38 @@ import eu.mcomputing.mobv.mobvzadanie.viewmodels.FeedViewModel
 import eu.mcomputing.mobv.mobvzadanie.R
 import eu.mcomputing.mobv.mobvzadanie.adapters.FeedAdapter
 import eu.mcomputing.mobv.mobvzadanie.adapters.MyItem
+import eu.mcomputing.mobv.mobvzadanie.databinding.FragmentFeedBinding
 
 class FeedFragment : Fragment(R.layout.fragment_feed),
     CustomBottomNavLayout.OnNavIconClickListener {
 
+    private var binding: FragmentFeedBinding? = null
     private lateinit var viewModel: FeedViewModel
 
-
     val TAG = "FeedFragment"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity())[FeedViewModel::class.java]
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val bottomNav = view.findViewById<CustomBottomNavLayout>(R.id.feedFragment)
-        bottomNav.setOnNavIconClickListener(this)  // Set the listener
-
-        viewModel = ViewModelProvider(this)[FeedViewModel::class.java]
-
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-
-        val feedAdapter = FeedAdapter()
-        recyclerView.adapter = feedAdapter
-
-        val email = arguments?.getString("email")
-
-        viewModel.feedItems.observe(viewLifecycleOwner) { items ->
-            feedAdapter.updateItems(items)
+        binding = FragmentFeedBinding.bind(view).apply {
+            lifecycleOwner = viewLifecycleOwner
+            model = viewModel
+        }.also { bnd->
+            bnd.btnGenerate.setOnClickListener {
+                viewModel.updateItems()
+            }
         }
-
-        viewModel.updateItems(
-            listOf(
-                MyItem(1, R.drawable.ic_person, "Prvy"),
-                MyItem(2, R.drawable.ic_person, "Druhy"),
-                MyItem(3, R.drawable.ic_person, "Treti"),
-                MyItem(4, R.drawable.ic_person, "Stvrty"),
-                MyItem(5, R.drawable.ic_person, "Piaty"),
-                MyItem(6, R.drawable.ic_person, "Siesty"),
-                MyItem(7, R.drawable.ic_person, "Siedmy"),
-                MyItem(8, R.drawable.ic_person, "Osmy"),
-                MyItem(9, R.drawable.ic_person, "Deviaty"),
-                MyItem(10, R.drawable.ic_person, "Desiaty"),
-                MyItem(11, R.drawable.ic_person, "Jedenasty")
-            )
-        )
-
     }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
+    }
+
 
     override fun onMapClick() {
         findNavController().navigate(R.id.mapFragment)
