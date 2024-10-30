a91c0276df94c5d86eb7e86af3cc2b5be6334e17
Marian Figula <majofigulam3@gmail.com>
2024-10-30 09:43:38 +0100

    feat: api service for getUser and refreshToken

--- app/src/main/java/eu/mcomputing/mobv/mobvzadanie/ApiService.kt
+++ app/src/main/java/eu/mcomputing/mobv/mobvzadanie/ApiService.kt
@@ -4,9 +4,12 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.HeaderMap
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface ApiService {
     @Headers("x-apikey: c95332ee022df8c953ce470261efc695ecf3e784")
@@ -16,6 +19,18 @@ interface ApiService {
     @Headers("x-apikey: c95332ee022df8c953ce470261efc695ecf3e784")
     @POST("user/login.php")
     suspend fun loginUser(@Body userInfo: UserLogin): Response<LoginResponse>
+
+    @GET("user/get.php")
+    suspend fun getUser(
+        @HeaderMap header: Map<String, String>,
+        @Query("id") id: String
+    ): Response<UserResponse>
+
+    @POST("user/refresh.php")
+    suspend fun refreshToken(
+        @HeaderMap header: Map<String, String>,
+        @Body refreshInfo: RefreshTokenRequest
+    ): Response<RefreshTokenResponse>
 
     @Headers("x-apikey: c95332ee022df8c953ce470261efc695ecf3e784")
     @POST("user/reset.php")
@@ -44,6 +59,7 @@ interface ApiService {
     }
 }
 
+// create.php
 data class UserRegistration(
     val name: String,
     val email: String,
@@ -56,7 +72,7 @@ data class RegistrationResponse(
     val refresh: String
 )
 
-
+// login.php
 data class UserLogin(
     val name: String,
     val password: String
@@ -68,8 +84,28 @@ data class LoginResponse(
     val refresh: String
 )
 
+// get.php
+// TODO: mozno uid miesto id
+data class UserResponse(
+    val id: String,
+    val name: String,
+    val photo: String
+)
+
+// refresh.php
+data class RefreshTokenRequest(val refresh: String)
+
+data class RefreshTokenResponse(
+    val uid: String,
+    val access: String,
+    val refresh: String
+)
+
+// reset.php
 data class ResetPasswordRequest(val email: String)
 data class ResetPasswordResponse(val status: String, val message: String? = null)
 
+
+// password.php
 data class ChangePasswordRequest(val oldPassword: String, val newPassword: String)
 data class ChangePasswordResponse(val status: String)
